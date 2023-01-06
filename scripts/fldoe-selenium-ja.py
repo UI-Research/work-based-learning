9358625e3b1f985390b0433929f1cadcf0945638
judah-axelrod <judahaxelrod@gmail.com>
2023-01-06 17:01:08 -0500

    First 500 courses for first discipline

--- scripts/fldoe-selenium-ja.py
+++ scripts/fldoe-selenium-ja.py
@@ -37,13 +37,25 @@ def get_discipline_courses(discipline_link):
     select_dropdown(identifier='//*[@id="ContentPlaceHolder1_ddlPageSize"]', value='500')
     course_links_clickable = EC.element_to_be_clickable((By.CLASS_NAME, 'btn-link'))
     WebDriverWait(driver, timeout=15).until(course_links_clickable)
-    course_links = driver.find_elements("xpath", '//*[@class="btn-link"]')
-    for (j, course_link) in enumerate(course_links)
-        print(j)
-        get_course_info(course_link)
+
+    n_course_links = len(driver.find_elements("xpath", '//*[@class="btn-link"]'))
+    
+    for j in range(n_course_links):
+        if j < 499:
+            continue
+        course_links_clickable = EC.element_to_be_clickable((By.CLASS_NAME, 'btn-link'))
+        WebDriverWait(driver, timeout=15).until(course_links_clickable)
+        print(f'     Course {j}')
+        course_links = driver.find_elements("xpath", '//*[@class="btn-link"]')
+        course_link = course_links[j]
+        print(len(course_links))
+        # print(course_link)
+        go_back = 1 if j < n_course_links - 1 else 2 # Need to tweak this to handle multiple pages of courses
+        get_course_info(course_link, go_back=go_back)
 
 ### Click on course link and retrieve table with description in json format
-def get_course_info(course_link):
+def get_course_info(course_link, go_back=1):
+    print('Entered here')
     # Click on course
     course_id = course_link.get_attribute('id')
     course_link_clickable = EC.element_to_be_clickable((By.ID, course_id))
@@ -54,7 +66,9 @@ def get_course_info(course_link):
     dict = {}
     dict = get_kv(form)
     kv_to_json(dict)
-    driver.execute_script("window.history.go(-1)")
+    print('Going back')
+    driver.execute_script(f"window.history.go(-{go_back})")
+    print('Went back')
 
 ### Get key value pairs to construct course json file
 def get_kv(form):
@@ -100,7 +114,12 @@ if __name__ == '__main__':
     element_clickable = EC.element_to_be_clickable((By.CLASS_NAME, 'btn-link'))
     element = WebDriverWait(driver, timeout=15).until(element_clickable)
 
-
-    discipline_links = driver.find_elements("xpath", '//*[@class="btn-link"]')
-    courses = get_discipline_courses(discipline_links[0])
+    
+    n_discipline_links = len(driver.find_elements("xpath", '//*[@class="btn-link"]')) # number of course disciplines on the site
+    for i in range(n_discipline_links):
+        print(f'Discipline {i}')
+        discipline_link_clickable = EC.element_to_be_clickable((By.CLASS_NAME, 'btn-link'))
+        WebDriverWait(driver, timeout=15).until(discipline_link_clickable)
+        discipline_link = driver.find_elements("xpath", '//*[@class="btn-link"]')[i]
+        get_discipline_courses(discipline_link)
     driver.quit()
